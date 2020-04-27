9133eaf69555da83e6786ea856a159db9af2525e
mits58 <ynakano927@gmail.com>
2020-04-27 13:29:22 +0900

    changed

--- graph.py
+++
@@ -1,7 +0,0 @@
-
-class Graph(object):
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        pass
--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,4 @@
 import sys
 
 # all source file is src folder
-sys.path.append('src')
+sys.path.append('pygraph')
