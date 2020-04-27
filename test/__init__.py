40262ded8e90aafe87cc1047a24e9fa4f008795b
mits58 <ynakano927@gmail.com>
2020-04-27 13:19:44 +0900

    add some sample scripts about Travis CI

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+import sys
+
+# all source file is src folder
+sys.path.append('src')
---
+++ test/module/test_add.py
@@ -0,0 +1,12 @@
+import unittest
+
+from module.add import add
+
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
